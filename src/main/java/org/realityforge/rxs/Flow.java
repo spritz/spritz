a734ef08ada0774792fb9cf2452584f642915e92
Peter Donald <peter@realityforge.org>
2018-09-17 15:31:34 +1000

    Add FLow that is essentially a reimplementation of similar interfaces in Java9 adapter for GWT

---
+++ src/main/java/org/realityforge/rxs/Flow.java
@@ -0,0 +1,43 @@
+package org.realityforge.rxs;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This is re-implements the API added to java 9 which is a reimplementation of the reactive streams API with
+ * the exception that the long was converted to an int to reduce overhead when compiling to js.
+ */
+public final class Flow
+{
+  private Flow()
+  {
+  }
+
+  public interface Subscription
+  {
+    void request( int count );
+
+    void cancel();
+  }
+
+  public interface Subscriber<T>
+  {
+    void onSubscribe( @Nonnull Subscription subscription );
+
+    void onNext( @Nonnull T item );
+
+    void onError( @Nonnull Throwable throwable );
+
+    void onComplete();
+  }
+
+  @FunctionalInterface
+  public interface Publisher<T>
+  {
+    void subscribe( @Nonnull Subscriber<? super T> subscriber );
+  }
+
+  public interface Processor<T, R>
+    extends Subscriber<T>, Publisher<R>
+  {
+  }
+}
