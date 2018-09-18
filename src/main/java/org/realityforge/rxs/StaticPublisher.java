add5cd7457fe95e7d52047f6bd3c92ec5e130b48
Peter Donald <peter@realityforge.org>
2018-09-18 20:09:30 +1000

    Extract inner class and make sure class follows the rules outlined in react-streaming API

--- src/main/java/org/realityforge/rxs/StaticPublisher.java
+++ src/main/java/org/realityforge/rxs/StaticPublisher.java
@@ -7,9 +7,8 @@ public final class StaticPublisher<T>
   implements Flow.Publisher<T>
 {
   private final T[] _data;
-  private int _offset;
 
-  public StaticPublisher( @Nonnull final T[] data )
+  StaticPublisher( @Nonnull final T[] data )
   {
     _data = Objects.requireNonNull( data );
   }
@@ -17,30 +16,47 @@ public final class StaticPublisher<T>
   @Override
   public void subscribe( @Nonnull final Flow.Subscriber<? super T> subscriber )
   {
-    final Flow.Subscription subscription = new Flow.Subscription()
+    subscriber.onSubscribe( new StaticSubscription( subscriber ) );
+  }
+
+  private class StaticSubscription
+    implements Flow.Subscription
+  {
+    private final Flow.Subscriber<? super T> _subscriber;
+    private int _offset;
+
+    StaticSubscription( final Flow.Subscriber<? super T> subscriber )
     {
-      @Override
-      public void request( final int count )
+      _subscriber = subscriber;
+    }
+
+    @Override
+    public void request( final int count )
+    {
+      final int requestEnd = _offset + count;
+      if ( requestEnd <= _data.length )
       {
-        final int requestEnd = _offset + count;
-        final int endIndex = Math.min( requestEnd, _data.length );
-        for ( int i = _offset; i < endIndex; i++ )
+        for ( int i = _offset; i < requestEnd; i++ )
         {
-          subscriber.onNext( _data[ i ] );
+          _subscriber.onNext( _data[ i ] );
         }
-        _offset = endIndex;
+        _offset = requestEnd;
         if ( _offset == _data.length )
         {
-          subscriber.onComplete();
+          _subscriber.onComplete();
+          _offset = _data.length + 1;
         }
       }
+    }
 
-      @Override
-      public void cancel()
+    @Override
+    public void cancel()
+    {
+      if ( _offset <= _data.length )
       {
-        _offset = _data.length;
+        _subscriber.onComplete();
+        _offset = _data.length + 1;
       }
-    };
-    subscriber.onSubscribe( new ValidatingSubscription( (ValidatingSubscriber<?>) subscriber, subscription ) );
+    }
   }
 }
