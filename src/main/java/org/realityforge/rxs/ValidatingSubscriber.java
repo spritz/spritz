34b5c274853d0af309fce3a6c5bb66e98480a5f9
Peter Donald <peter@realityforge.org>
2018-09-20 20:03:47 +1000

    Move subscription into class as inner class

--- src/main/java/org/realityforge/rxs/ValidatingSubscriber.java
+++ src/main/java/org/realityforge/rxs/ValidatingSubscriber.java
@@ -50,7 +50,7 @@ public final class ValidatingSubscriber<T>
     {
       pushContext( this );
       _state = State.SUBSCRIBED;
-      _target.onSubscribe( new ValidatingSubscription( this, subscription ) );
+      _target.onSubscribe( new WorkerSubscription<T>( this, subscription ) );
     }
     catch ( final Throwable throwable )
     {
@@ -169,18 +169,18 @@ public final class ValidatingSubscriber<T>
   }
 
   @Nonnull
-  State getState()
+  private State getState()
   {
     return _state;
   }
 
-  static boolean hasContext()
+  private static boolean hasContext()
   {
     return !c_subscriberContext.isEmpty();
   }
 
   @Nonnull
-  static ValidatingSubscriber<?> currentContext()
+  private static ValidatingSubscriber<?> currentContext()
   {
     if ( BrainCheckConfig.checkInvariants() )
     {
@@ -211,6 +211,89 @@ public final class ValidatingSubscriber<T>
       invariant( () -> removed == subscriber,
                  () -> "Rxs-0011: Invoking Subscriber.popContext(...) popped subscriber '" + removed +
                        "' but was expecting subscriber '" + subscriber + "'." );
+    }
+  }
+
+  private static final class WorkerSubscription<T>
+    implements Flow.Subscription
+  {
+    @Nonnull
+    private final ValidatingSubscriber<T> _subscriber;
+    @Nonnull
+    private final Flow.Subscription _subscription;
+    private boolean _cancelled;
+
+    WorkerSubscription( @Nonnull final ValidatingSubscriber<T> subscriber, @Nonnull final Flow.Subscription subscription )
+    {
+      _subscriber = Objects.requireNonNull( subscriber );
+      _subscription = Objects.requireNonNull( subscription );
+    }
+
+    @Override
+    public void request( final int count )
+    {
+      if ( BrainCheckConfig.checkInvariants() )
+      {
+        invariant( ValidatingSubscriber::hasContext,
+                   () -> "Rxs-0013: Invoking Subscription.request(...) but not in the context of a subscriber." );
+        final ValidatingSubscriber<?> subscriber = currentContext();
+        invariant( () -> subscriber == _subscriber,
+                   () -> "Rxs-0014: Invoking Subscription.request(...) in the context of subscriber '" + subscriber +
+                         "' but expected to be in the context of subscriber '" + _subscriber + "'." );
+        invariant( () -> State.SUBSCRIBED == _subscriber.getState(),
+                   () -> "Rxs-0015: Invoking Subscription.request(...) when the subscriber '" + subscriber +
+                         "' is not in the expected SUBSCRIBED state." );
+        invariant( () -> count > 0,
+                   () -> "Rxs-0021: Invoking Subscription.request(...) with count " +
+                         count + " but count must be a positive number." );
+      }
+      if ( !_cancelled )
+      {
+        try
+        {
+          _subscription.request( count );
+        }
+        catch ( final Throwable t )
+        {
+          if ( BrainCheckConfig.checkInvariants() )
+          {
+            fail( () -> "Rxs-0017: Invoking Subscription.request(...) incorrectly threw an exception. " +
+                        "Exception:\n" + ErrorUtil.throwableToString( t ) );
+          }
+          throw t;
+        }
+      }
+    }
+
+    @Override
+    public void cancel()
+    {
+      if ( BrainCheckConfig.checkInvariants() )
+      {
+        invariant( ValidatingSubscriber::hasContext,
+                   () -> "Rxs-0018: Invoking Subscription.cancel(...) but not in the context of a subscriber." );
+        final ValidatingSubscriber<?> subscriber = currentContext();
+        invariant( () -> subscriber == _subscriber,
+                   () -> "Rxs-0019: Invoking Subscription.cancel(...) in the context of subscriber '" + subscriber +
+                         "' but expected to be in the context of subscriber '" + _subscriber + "'." );
+      }
+      if ( !_cancelled )
+      {
+        try
+        {
+          _cancelled = true;
+          _subscription.cancel();
+        }
+        catch ( final Throwable t )
+        {
+          if ( BrainCheckConfig.checkInvariants() )
+          {
+            fail( () -> "Rxs-0020: Invoking Subscription.cancel(...) incorrectly threw an exception. " +
+                        "Exception:\n" + ErrorUtil.throwableToString( t ) );
+          }
+          throw t;
+        }
+      }
     }
   }
 }
