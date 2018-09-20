1fc2aba96f0b1302e03b1b8df404ee59ee3a0a7d
Peter Donald <peter@realityforge.org>
2018-09-20 17:51:38 +1000

    Rename class

--- src/main/java/org/realityforge/rxs/PredicateFilterPublisher.java
+++ src/main/java/org/realityforge/rxs/PredicateFilterPublisher.java
@@ -20,17 +20,17 @@ final class PredicateFilterPublisher<T>
   @Override
   public void subscribe( @Nonnull final Flow.Subscriber<? super T> subscriber )
   {
-    getUpstream().subscribe( new PredicateFilterSubscriber<>( subscriber, _predicate ) );
+    getUpstream().subscribe( new PredicateFilterSubscription<>( subscriber, _predicate ) );
   }
 
-  private static final class PredicateFilterSubscriber<T>
+  private static final class PredicateFilterSubscription<T>
     extends AbstractFilterSubscription<T>
   {
     @Nonnull
     private final Predicate<? super T> _predicate;
 
-    PredicateFilterSubscriber( @Nonnull final Flow.Subscriber<? super T> subscriber,
-                               @Nonnull final Predicate<? super T> predicate )
+    PredicateFilterSubscription( @Nonnull final Flow.Subscriber<? super T> subscriber,
+                                 @Nonnull final Predicate<? super T> predicate )
     {
       super( subscriber );
       _predicate = Objects.requireNonNull( predicate );
