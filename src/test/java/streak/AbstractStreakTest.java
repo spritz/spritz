1d3dcf891fc2f290cf39ad68e982bb6d097e70d9
Peter Donald <peter@realityforge.org>
2018-09-22 20:34:05 +1000

    Remove unnecessary throws

--- src/test/java/streak/AbstractStreakTest.java
+++ src/test/java/streak/AbstractStreakTest.java
@@ -10,14 +10,12 @@ public abstract class AbstractStreakTest
 {
   @BeforeMethod
   protected void beforeTest()
-    throws Exception
   {
     BrainCheckTestUtil.resetConfig( false );
   }
 
   @AfterMethod
   protected void afterTest()
-    throws Exception
   {
     BrainCheckTestUtil.resetConfig( true );
   }
