cde8beac3bd5a66add8f061633aa703ba785888b
Peter Donald <peter@realityforge.org>
2018-10-13 22:08:12 +1100

    Add mechanism for composing and terminating reactive streams

--- src/main/java/streak/PublisherExtension.java
+++ src/main/java/streak/PublisherExtension.java
@@ -3,18 +3,19 @@ package streak;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 
 public interface PublisherExtension<T>
 {
   default Flow.Publisher<T> filter( @Nonnull final Predicate<T> predicate )
   {
-    return new PredicateFilterPublisher<>( self(), predicate );
+    return compose( p -> new PredicateFilterPublisher<>( p, predicate ) );
   }
 
   default Flow.Publisher<T> take( final int count )
   {
-    return new TakeFilterPublisher<>( self(), count );
+    return compose( p -> new TakeFilterPublisher<>( p, count ) );
   }
 
   default Flow.Publisher<T> first()
@@ -24,22 +25,33 @@ public interface PublisherExtension<T>
 
   default Flow.Publisher<T> skipUntil( @Nonnull final Predicate<T> predicate )
   {
-    return new SkipUntilPredicateFilterPublisher<>( self(), predicate );
+    return compose( p -> new SkipUntilPredicateFilterPublisher<>( p, predicate ) );
   }
 
   default Flow.Publisher<T> distinctInSuccession()
   {
-    return new DistinctInSuccessionFilterPublisher<>( self() );
+    return compose( DistinctInSuccessionFilterPublisher::new );
   }
 
   default <DownstreamT> Flow.Publisher<DownstreamT> map( @Nonnull final Function<T, DownstreamT> transform )
   {
-    return new MapPublisher<>( self(), transform );
+    return compose( p -> new MapPublisher<>( p, transform ) );
   }
 
   default void forEach( @Nonnull final Consumer<T> action )
   {
-    self().subscribe( new ForEachSubscriber<>( action ) );
+    terminate( () -> new ForEachSubscriber<>( action ) );
+  }
+
+  @Nonnull
+  default <DownstreamT> Flow.Publisher<DownstreamT> compose( @Nonnull final Function<Flow.Publisher<T>, Flow.Publisher<DownstreamT>> composeFunction )
+  {
+    return composeFunction.apply( self() );
+  }
+
+  default void terminate( @Nonnull final Supplier<Flow.Subscriber<T>> terminateFunction )
+  {
+    self().subscribe( terminateFunction.get() );
   }
 
   @Nonnull
