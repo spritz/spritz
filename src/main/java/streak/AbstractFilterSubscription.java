91656416e9e7b29c3019ab76202fbf962c813dbc
Peter Donald <peter@realityforge.org>
2018-10-14 19:30:11 +1100

    Dispose upstream on error

--- src/main/java/streak/AbstractFilterSubscription.java
+++ src/main/java/streak/AbstractFilterSubscription.java
@@ -47,6 +47,7 @@ abstract class AbstractFilterSubscription<T>
     catch ( final Throwable throwable )
     {
       onError( throwable );
+      getUpstream().dispose();
       return false;
     }
   }
