17ec98065050cb9fb4a1e66cdac0700d88d59b3a
Peter Donald <peter@realityforge.org>
2018-12-31 10:13:17 +1100

    Move filtering operators to separate interface

---
+++ src/main/java/streak/FilteringOperators2.java
@@ -0,0 +1,41 @@
+package streak;
+
+import java.util.function.Predicate;
+import javax.annotation.Nonnull;
+
+public interface FilteringOperators2<T>
+  extends StreamExtension<T>
+{
+  default Flow.Stream<T> filter( @Nonnull final Predicate<T> predicate )
+  {
+    return compose( p -> new PredicateFilterPublisher<>( p, predicate ) );
+  }
+
+  default Flow.Stream<T> take( final int count )
+  {
+    return compose( p -> new TakeFilterPublisher<>( p, count ) );
+  }
+
+  default Flow.Stream<T> first()
+  {
+    return take( 1 );
+  }
+
+  default Flow.Stream<T> dropWhile( @Nonnull final Predicate<T> predicate )
+  {
+    return compose( p -> new DropWhileOperator<>( p, predicate ) );
+  }
+
+  default Flow.Stream<T> skipUntil( @Nonnull final Predicate<T> predicate )
+  {
+    return dropWhile( predicate.negate() );
+  }
+
+  /**
+   * drops consecutive equal elements
+   */
+  default Flow.Stream<T> skipConsecutiveDuplicates()
+  {
+    return compose( SkipConsecutiveDuplicatesOperator::new );
+  }
+}
--- src/main/java/streak/PublisherExtension.java
+++ src/main/java/streak/PublisherExtension.java
@@ -2,46 +2,12 @@ package streak;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 
 public interface PublisherExtension<T>
-  extends StreamExtension<T>
+  extends StreamExtension<T>, FilteringOperators2<T>
 {
-  default Flow.Stream<T> filter( @Nonnull final Predicate<T> predicate )
-  {
-    return compose( p -> new PredicateFilterPublisher<>( p, predicate ) );
-  }
-
-  default Flow.Stream<T> take( final int count )
-  {
-    return compose( p -> new TakeFilterPublisher<>( p, count ) );
-  }
-
-  default Flow.Stream<T> first()
-  {
-    return take( 1 );
-  }
-
-  default Flow.Stream<T> dropWhile( @Nonnull final Predicate<T> predicate )
-  {
-    return compose( p -> new DropWhileOperator<>( p, predicate ) );
-  }
-
-  default Flow.Stream<T> skipUntil( @Nonnull final Predicate<T> predicate )
-  {
-    return dropWhile( predicate.negate() );
-  }
-
-  /**
-   * drops consecutive equal elements
-   */
-  default Flow.Stream<T> skipConsecutiveDuplicates()
-  {
-    return compose( SkipConsecutiveDuplicatesOperator::new );
-  }
-
   default <DownstreamT> Flow.Stream<DownstreamT> map( @Nonnull final Function<T, DownstreamT> transform )
   {
     return compose( p -> new MapPublisher<>( p, transform ) );
