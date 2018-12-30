6e46547cb165f64af8632ef0847e02041c47ff73
Peter Donald <peter@realityforge.org>
2018-12-31 10:33:20 +1100

    Add some basic documentation and a few alias methods

--- src/main/java/streak/FilteringOperators2.java
+++ src/main/java/streak/FilteringOperators2.java
@@ -1,35 +1,103 @@
 package streak;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 
+/**
+ * Operators for filtering elements from a stream.
+ */
 public interface FilteringOperators2<T>
   extends StreamExtension<T>
 {
+  /**
+   * Filter the elements emitted by this stream using the specified {@link Predicate}.
+   * Any elements that return {@code true} when passed to the {@link Predicate} will be
+   * emitted while all other elements will be dropped.
+
+   *
+   * @param predicate The predicate to apply to each element.
+   * @return the stream.
+   */
   @Nonnull
   default Flow.Stream<T> filter( @Nonnull final Predicate<T> predicate )
   {
     return compose( p -> new PredicateFilterPublisher<>( p, predicate ) );
   }
 
+  /**
+   * Truncate the stream, ensuring the stream is no longer than {@code maxSize} elements in length.
+   * If {@code maxSize} is reached then the element will be passed downstream, the downstream will be
+   * completed and then the upstream will be disposed. This method is an alias for {@link #limit(int)}
+   *
+   * @param maxSize The maximum number of elements returned by the stream.
+   * @return the stream.
+   * @see #limit(int)
+   */
   @Nonnull
-  default Flow.Stream<T> take( final int count )
+  default Flow.Stream<T> take( final int maxSize )
   {
-    return compose( p -> new TakeFilterPublisher<>( p, count ) );
+    return limit( maxSize );
   }
 
+  /**
+   * Truncate the stream, ensuring the stream is no longer than {@code maxSize} elements in length.
+   * If {@code maxSize} is reached then the element will be passed downstream, the downstream will be
+   * completed and then the upstream will be disposed. This method is an alias for {@link #take(int)}
+   *
+   * @param maxSize The maximum number of elements returned by the stream.
+   * @return the stream.
+   * @see #take(int)
+   */
+  @Nonnull
+  default Flow.Stream<T> limit( final int maxSize )
+  {
+    return compose( p -> new TakeFilterPublisher<>( p, maxSize ) );
+  }
+
+  /**
+   * Pass the first element downstream, complete the downstream and dispose the upstream.
+   * This method is an alias for {@link #take(int)} or {@link #limit(int)} where <code>1</code> is
+   * passed as the parameter.
+   *
+   * @return the stream.
+   * @see #take(int)
+   * @see #limit(int)
+   */
   @Nonnull
   default Flow.Stream<T> first()
   {
     return take( 1 );
   }
 
+  /**
+   * Drop elements from this stream until an element no longer match the supplied {@code predicate}.
+   * As long as the {@code predicate} returns true, no elements will be emitted from this stream. Once
+   * the first element is encountered for which the {@code predicate} returns false, all subsequent
+   * elements will be emitted, and the {@code predicate} will no longer be invoked. This is equivalent
+   * to {@link #skipUntil(Predicate)} if the predicate is negated.
+   *
+   * @param predicate The predicate.
+   * @return the stream.
+   * @see #skipUntil(Predicate)
+   */
   @Nonnull
   default Flow.Stream<T> dropWhile( @Nonnull final Predicate<T> predicate )
   {
     return compose( p -> new DropWhileOperator<>( p, predicate ) );
   }
 
+  /**
+   * Drop elements from this stream until an element matches the supplied {@code predicate}.
+   * As long as the {@code predicate} returns false, no elements will be emitted from this stream. Once
+   * the first element is encountered for which the {@code predicate} returns true, all subsequent
+   * elements will be emitted, and the {@code predicate} will no longer be invoked. This is equivalent
+   * to {@link #dropWhile(Predicate)} if the predicate is negated.
+   *
+   * @param predicate The predicate.
+   * @return the stream.
+   * @see #dropWhile(Predicate)
+   */
   @Nonnull
   default Flow.Stream<T> skipUntil( @Nonnull final Predicate<T> predicate )
   {
@@ -37,7 +105,12 @@ public interface FilteringOperators2<T>
   }
 
   /**
-   * drops consecutive equal elements
+   * Drops elements from the stream if they are equal to the previous element emitted in the stream.
+   * The elements are tested for equality using the {@link Objects#equals(Object, Object)} method.
+   * This method is an alias for {@link #skipConsecutiveDuplicates()}.
+   *
+   * @return the stream.
+   * @see #skipConsecutiveDuplicates()
    */
   @Nonnull
   default Flow.Stream<T> dropConsecutiveDuplicates()
@@ -45,6 +118,14 @@ public interface FilteringOperators2<T>
     return compose( DropConsecutiveDuplicatesOperator::new );
   }
 
+  /**
+   * Drops elements from the stream if they are equal to the previous element emitted in the stream.
+   * The elements are tested for equality using the {@link Objects#equals(Object, Object)} method.
+   * This method is an alias for {@link #dropConsecutiveDuplicates()}.
+   *
+   * @return the stream.
+   * @see #dropConsecutiveDuplicates()
+   */
   @Nonnull
   default Flow.Stream<T> skipConsecutiveDuplicates()
   {
