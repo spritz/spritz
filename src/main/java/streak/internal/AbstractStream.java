79a6c6043710d40d938e49a905492335165c8426
Peter Donald <peter@realityforge.org>
2019-01-01 10:17:41 +1100

    Add some minimal javadocs

--- src/main/java/streak/internal/AbstractStream.java
+++ src/main/java/streak/internal/AbstractStream.java
@@ -3,9 +3,15 @@ package streak.internal;
 import javax.annotation.Nonnull;
 import streak.Flow;
 
+/**
+ * A simple base class that can be used build streams.
+ */
 public abstract class AbstractStream<T>
   implements Flow.Stream<T>
 {
+  /**
+   * {@inheritDoc}
+   */
   @Nonnull
   @Override
   public final Flow.Stream<T> self()
