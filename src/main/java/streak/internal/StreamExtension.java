fa765913236896c141ac8a3f451d9f7815678475
Peter Donald <peter@realityforge.org>
2019-01-01 10:22:50 +1100

    Add some minimal javadocs

--- src/main/java/streak/internal/StreamExtension.java
+++ src/main/java/streak/internal/StreamExtension.java
@@ -11,12 +11,25 @@ import streak.Flow;
  */
 public interface StreamExtension<T>
 {
+  /**
+   * Compost this stream with another stream and return the new stream.
+   * This method is used to compose chains of stream operations.
+   *
+   * @param <DownstreamT> the type of element emitted by downstream stage.
+   * @param <S> the type of the downstream stage.
+   * @param function the function used to compose stream operations.
+   */
   @Nonnull
-  default <DownstreamT, S extends Flow.Stream<DownstreamT>> S compose( @Nonnull final Function<Flow.Stream<T>, S> composeFunction )
+  default <DownstreamT, S extends Flow.Stream<DownstreamT>> S compose( @Nonnull final Function<Flow.Stream<T>, S> function )
   {
-    return composeFunction.apply( new ValidatingStream<>( self() ) );
+    return function.apply( new ValidatingStream<>( self() ) );
   }
 
+  /**
+   * Return the reference to the stream that the extension is extending.
+   *
+   * @return the underlying stream.
+   */
   @Nonnull
   Flow.Stream<T> self();
 }
