a124cb95720d490344abc2a2a052c7ae3e164078
Peter Donald <peter@realityforge.org>
2018-12-31 20:33:07 +1100

    Add mapTo operator and add some basic javadocs

--- src/main/java/streak/internal/transforming/TransformingOperators.java
+++ src/main/java/streak/internal/transforming/TransformingOperators.java
@@ -6,16 +6,36 @@ import streak.Flow;
 import streak.internal.StreamExtension;
 
 /**
- * Operators for filtering elements from a stream.
+ * Operators for transforming elements from one form to another.
  *
- * @param <T> The type of the elements that the stream consumes and emits.
+ * @param <T> the type of the elements that the stream consumes.
  */
 public interface TransformingOperators<T>
   extends StreamExtension<T>
 {
+  /**
+   * Transform elements emitted by this stream using the {@code transform} function.
+   *
+   * @param <DownstreamT> the type of the elements that the {@code transform} function emits.
+   * @param transform     the function to use to map the elements.
+   * @return the stream.
+   */
   @Nonnull
   default <DownstreamT> Flow.Stream<DownstreamT> map( @Nonnull final Function<T, DownstreamT> transform )
   {
     return compose( p -> new MapPublisher<>( p, transform ) );
+  }
+
+  /**
+   * Transform elements emitted by this stream to a constant {@code value}.
+   *
+   * @param <DownstreamT> the type of the constant value emitted.
+   * @param value         the constant value to emit.
+   * @return the stream.
+   */
+  @Nonnull
+  default <DownstreamT> Flow.Stream<DownstreamT> mapTo( final DownstreamT value )
+  {
+    return map( v -> value );
   }
 }
