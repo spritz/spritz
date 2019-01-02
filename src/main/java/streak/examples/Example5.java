5fc70bbdd6f1df5b079d6aa2674fa11375a89ccd
Peter Donald <peter@realityforge.org>
2019-01-03 10:21:36 +1100

    Fix compile error

--- src/main/java/streak/examples/Example5.java
+++ src/main/java/streak/examples/Example5.java
@@ -7,10 +7,11 @@ import streak.StreakContext;
 
 public class Example5
 {
+  @SuppressWarnings( "unchecked" )
   public static void main( String[] args )
   {
     final StreakContext context = Streak.context();
-    Streak
+    context
       .concat( context.of( 1, 2, 3 ), context.of( 4, 5, 6 ), context.of( 7, 8, 9 ) )
       .skip( 4 )
       .subscribe( new Flow.Subscriber<Integer>()
