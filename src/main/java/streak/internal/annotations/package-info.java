d7315c0a7fdcca266711c9b936875212552c1b5c
Peter Donald <peter@realityforge.org>
2019-01-14 15:34:45 +1100

    Add some "package documentation" to indicate internal packages should not be used

---
+++ src/main/java/streak/internal/annotations/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Annotations that are used internally within Streak.
+ * These should NOT be used outside of Stream and can change at any time.
+ */
+package streak.internal.annotations;
---
+++ src/main/java/streak/internal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Streak internal support code.
+ * Code under this package can change at any time and should not be referenced outside this package.
+ */
+package streak.internal;
