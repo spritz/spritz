2c4c5cb25d788ac3582e0c991044c060e7925016
Peter Donald <peter@realityforge.org>
2019-01-29 08:28:49 +1100

    Describe package

---
+++ core/src/main/java/spritz/internal/vpu/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Spritz internal support code for driving VirtualProcessorUnit infrastructure.
+ * Code under this package can change at any time and should not be referenced outside this package.
+ */
+package spritz.internal.vpu;
