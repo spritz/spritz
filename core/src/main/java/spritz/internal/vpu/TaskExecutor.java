e35346ec21a290b53462b374cd2ea433c55de448
Peter Donald <peter@realityforge.org>
2019-01-28 22:05:43 +1100

    Add method to queue as part of the interface

--- core/src/main/java/spritz/internal/vpu/TaskExecutor.java
+++ core/src/main/java/spritz/internal/vpu/TaskExecutor.java
@@ -1,5 +1,7 @@
 package spritz.internal.vpu;
 
+import javax.annotation.Nonnull;
+
 /**
  * Interface via which the {@link VirtualProcessorUnit} executes tasks.
  * Each time the {@link VirtualProcessorUnit} is activated it will invoke {@link #executeTasks()}
@@ -12,4 +14,12 @@ public interface TaskExecutor
    * Execute the tasks for a single activation.
    */
   void executeTasks();
+
+  /**
+   * Queue task for execution and enable the executor for activation if necessary.
+   * The task must not be already queued.
+   *
+   * @param task the task.
+   */
+  void queueTask( @Nonnull Task task );
 }
