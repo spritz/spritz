1382e72cd3fd908b46ef17dd31b104e2c22e7815
Peter Donald <peter@realityforge.org>
2019-01-31 09:01:05 +1100

    Log during onComplete

--- examples/src/main/java/spritz/examples/Example30.java
+++ examples/src/main/java/spritz/examples/Example30.java
@@ -14,11 +14,14 @@ public class Example30
       .range( 42, 1 )
       .onSubscribe( s -> System.out.println( "onSubscribe1 on Thread: " + Thread.currentThread().getName() ) )
       .peek( v -> System.out.println( "Peek1 on Thread: " + Thread.currentThread().getName() ) )
+      .onComplete( () -> System.out.println( "onComplete1 on Thread: " + Thread.currentThread().getName() ) )
       .subscribeOn( FakeTaskExecutor.VPU1 )
       .onSubscribe( s -> System.out.println( "onSubscribe2 on Thread: " + Thread.currentThread().getName() ) )
       .peek( v -> System.out.println( "Peek2 on Thread: " + Thread.currentThread().getName() ) )
+      .onComplete( () -> System.out.println( "onComplete2 on Thread: " + Thread.currentThread().getName() ) )
       .subscribeOn( FakeTaskExecutor.VPU2 )
       .peek( v -> System.out.println( "Peek3 on Thread: " + Thread.currentThread().getName() ) )
+      .onComplete( () -> System.out.println( "onComplete3 on Thread: " + Thread.currentThread().getName() ) )
       .onTerminate( () -> complete.set( true ) )
       .subscribe( new LoggingSubscriber<>() );
 
