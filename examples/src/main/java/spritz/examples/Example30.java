85bf7ed8223df31f436c9b2c1687dde33f487463
Peter Donald <peter@realityforge.org>
2019-01-31 08:59:53 +1100

    Terminate correctly

--- examples/src/main/java/spritz/examples/Example30.java
+++ examples/src/main/java/spritz/examples/Example30.java
@@ -1,5 +1,6 @@
 package spritz.examples;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import spritz.Stream;
 import spritz.internal.vpu.example.FakeTaskExecutor;
 
@@ -8,16 +9,23 @@ public class Example30
   public static void main( String[] args )
     throws Exception
   {
+    final AtomicBoolean complete = new AtomicBoolean();
     Stream
       .range( 42, 1 )
-      .onSubscribe( s -> System.out.println( "onSubscribe on Thread: " + Thread.currentThread().getName() ) )
+      .onSubscribe( s -> System.out.println( "onSubscribe1 on Thread: " + Thread.currentThread().getName() ) )
       .peek( v -> System.out.println( "Peek1 on Thread: " + Thread.currentThread().getName() ) )
       .subscribeOn( FakeTaskExecutor.VPU1 )
-      .onSubscribe( s -> System.out.println( "onSubscribe on Thread: " + Thread.currentThread().getName() ) )
+      .onSubscribe( s -> System.out.println( "onSubscribe2 on Thread: " + Thread.currentThread().getName() ) )
       .peek( v -> System.out.println( "Peek2 on Thread: " + Thread.currentThread().getName() ) )
       .subscribeOn( FakeTaskExecutor.VPU2 )
       .peek( v -> System.out.println( "Peek3 on Thread: " + Thread.currentThread().getName() ) )
+      .onTerminate( () -> complete.set( true ) )
       .subscribe( new LoggingSubscriber<>() );
-    Thread.sleep( 1000 );
+
+    while ( !complete.get() )
+    {
+      Thread.sleep( 10 );
+    }
+    System.exit( 0 );
   }
 }
