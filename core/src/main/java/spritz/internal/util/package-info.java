d72be8d7e7871c2f9eba55034b18355c2e58579e
Peter Donald <peter@realityforge.org>
2019-02-05 10:22:28 +1100

    Add package documentation

---
+++ core/src/main/java/spritz/internal/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Spritz internal support code containing data structures and general utilities.
+ * Code under this package can change at any time and should not be referenced outside this package.
+ */
+package spritz.internal.util;
