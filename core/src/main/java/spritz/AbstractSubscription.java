54d0f2cf755aed74515aee8d44489c9c36562d10
Peter Donald <peter@realityforge.org>
2019-02-21 09:11:34 +1100

    Add mechanisms for getting names of subscriptions, derived from owning streams name

--- core/src/main/java/spritz/AbstractSubscription.java
+++ core/src/main/java/spritz/AbstractSubscription.java
@@ -2,6 +2,7 @@ package spritz;
 
 import java.util.Objects;
 import javax.annotation.Nonnull;
+import static org.realityforge.braincheck.Guards.*;
 
 /**
  * Abstract subscription implementation for the common scenario where
@@ -70,5 +71,39 @@ abstract class AbstractSubscription<T, S extends Stream<T>>
 
   void doCancel()
   {
+  }
+
+  /**
+   * Return the name of the subscription.
+   * This method should NOT be invoked unless {@link Spritz#areNamesEnabled()} returns <code>true</code>.
+   *
+   * @return the name of the subscription.
+   */
+  @Nonnull
+  public final String getName()
+  {
+    if ( Spritz.shouldCheckApiInvariants() )
+    {
+      apiInvariant( Spritz::areNamesEnabled,
+                    () -> "Spritz-0010: Subscription.getName() invoked when Spritz.areNamesEnabled() is false" );
+    }
+    return getStream().getName();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  public final String toString()
+  {
+    if ( Spritz.areNamesEnabled() )
+    {
+      return "Subscription[" + getName() + "]";
+    }
+    else
+    {
+      return super.toString();
+    }
   }
 }
