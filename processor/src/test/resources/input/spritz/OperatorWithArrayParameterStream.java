31cc586f89cdca9bc95298eece27f98120a539a0
Peter Donald <peter@realityforge.org>
2019-02-20 17:21:56 +1100

    Suppress warning

--- processor/src/test/resources/input/spritz/OperatorWithArrayParameterStream.java
+++ processor/src/test/resources/input/spritz/OperatorWithArrayParameterStream.java
@@ -7,6 +7,7 @@ import spritz.internal.annotations.MetaDataSource;
 @MetaDataSource
 public interface OperatorWithArrayParameterStream<T>
 {
+  @SuppressWarnings( "unchecked" )
   @DocCategory( DocCategory.Type.PEEKING )
   default OperatorWithArrayParameterStream<T> someOperator( @Nonnull final OperatorWithArrayParameterStream<T>... somePredicate )
   {
