5bba8b95ea211ea59bbd3a11c41cc245b5c85f9f
Peter Donald <peter@realityforge.org>
2019-03-01 16:59:44 +1100

    Remove accidentally committed test

--- core/src/test/java/spritz/StaticStreamSourceTest.java
+++ core/src/test/java/spritz/StaticStreamSourceTest.java
@@ -1,6 +1,5 @@
 package spritz;
 
-import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -19,39 +18,5 @@ public class StaticStreamSourceTest
   {
     final Stream<Integer> stream = Stream.of( "dataIn()", 1, 2, 3 );
     assertEquals( stream.getName(), "dataIn()" );
-  }
-
-  @Test
-  public void x()
-  {
-    final Stream<Integer> stream = Stream.of( 1, 2, 3 );
-    assertEquals( stream.getName(), "of(1, 2, 3)" );
-
-    stream.subscribe( new Subscriber<Integer>()
-    {
-      @Override
-      public void onSubscribe( @Nonnull final Subscription subscription )
-      {
-        assertEquals( subscription.toString(), "of(1, 2, 3)" );
-      }
-
-      @Override
-      public void onNext( @Nonnull final Integer item )
-      {
-
-      }
-
-      @Override
-      public void onError( @Nonnull final Throwable error )
-      {
-        fail( "Unexpected error signal" );
-      }
-
-      @Override
-      public void onComplete()
-      {
-
-      }
-    } );
   }
 }
