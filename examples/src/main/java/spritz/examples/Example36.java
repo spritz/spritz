0d83c0bdb71a4dcddd8876b0648c27eadb9014dd
Peter Donald <peter@realityforge.org>
2019-03-04 22:57:08 +1100

    Formatting cleanup

--- examples/src/main/java/spritz/examples/Example36.java
+++ examples/src/main/java/spritz/examples/Example36.java
@@ -1,6 +1,5 @@
 package spritz.examples;
 
-import java.util.function.Consumer;
 import spritz.Stream;
 
 public class Example36
@@ -9,17 +8,17 @@ public class Example36
   {
     final Stream<Integer> ticker$ = Stream
       .periodic( 1000 )
-      .takeUntil( v11 -> v11 > 10 ).peek( (Consumer<? super Integer>) v12 -> System.out.println( "Tick " + v12 ) )
+      .takeUntil( v11 -> v11 > 10 ).peek( v12 -> System.out.println( "Tick " + v12 ) )
       .publish();
 
     ticker$.filter( v4 -> v4 % 2 == 0 )
-      .peek( (Consumer<? super Integer>) v5 -> System.out.println( "T1=" + v5 ) )
+      .peek( v5 -> System.out.println( "T1=" + v5 ) )
       .subscribe( new LoggingSubscriber<>() );
     ticker$.filter( v2 -> v2 % 3 == 0 )
-      .peek( (Consumer<? super Integer>) v3 -> System.out.println( "T2=" + v3 ) )
+      .peek( v3 -> System.out.println( "T2=" + v3 ) )
       .subscribe( new LoggingSubscriber<>() );
     ticker$.filter( v -> v % 4 == 0 )
-      .peek( (Consumer<? super Integer>) v1 -> System.out.println( "T3=" + v1 ) )
+      .peek( v1 -> System.out.println( "T3=" + v1 ) )
       .subscribe( new LoggingSubscriber<>() );
 
     ExampleUtil.run( ticker$ );
