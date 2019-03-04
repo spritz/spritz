92ce28d02b4b3441dbea0c13a18edd318a42f6ee
Peter Donald <peter@realityforge.org>
2019-03-04 22:58:18 +1100

    Log the current VPU

--- examples/src/main/java/spritz/examples/LoggingSubscriber.java
+++ examples/src/main/java/spritz/examples/LoggingSubscriber.java
@@ -1,6 +1,7 @@
 package spritz.examples;
 
 import javax.annotation.Nonnull;
+import spritz.Scheduler;
 import spritz.Subscriber;
 import spritz.Subscription;
 
@@ -10,24 +11,24 @@ final class LoggingSubscriber<T>
   @Override
   public void onSubscribe( @Nonnull final Subscription subscription )
   {
-    System.out.println( "onSubscribe(" + subscription + ")" );
+    System.out.println( "onSubscribe(" + subscription + ") on " + Scheduler.currentVpu().getName() );
   }
 
   @Override
   public void onNext( @Nonnull final T item )
   {
-    System.out.println( "onNext(" + item + ")" );
+    System.out.println( "onNext(" + item + ") on " + Scheduler.currentVpu().getName() );
   }
 
   @Override
   public void onError( @Nonnull final Throwable error )
   {
-    System.out.println( "onError(" + error + ")" );
+    System.out.println( "onError(" + error + ") on " + Scheduler.currentVpu().getName() );
   }
 
   @Override
   public void onComplete()
   {
-    System.out.println( "onComplete()" );
+    System.out.println( "onComplete() on " + Scheduler.currentVpu().getName() );
   }
 }
