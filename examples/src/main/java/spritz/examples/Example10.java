24cec43c615761176e16ce02ad0579deb41211a3
Peter Donald <peter@realityforge.org>
2019-03-06 12:57:36 +1100

    Whitespace

--- examples/src/main/java/spritz/examples/Example10.java
+++ examples/src/main/java/spritz/examples/Example10.java
@@ -6,10 +6,11 @@ public class Example10
 {
   public static void main( String[] args )
   {
-    final Stream<String> stream = Stream
-      .periodic( 1000 )
-      .takeWhile( v -> v < 4 )
-      .switchMap( v -> Stream.periodic( 200 ).takeWhile( e -> e < 10 ).map( e -> v + "." + e ) );
+    final Stream<String> stream =
+      Stream
+        .periodic( 1000 )
+        .takeWhile( v -> v < 4 )
+        .switchMap( v -> Stream.periodic( 200 ).takeWhile( e -> e < 10 ).map( e -> v + "." + e ) );
     ExampleUtil.run( stream );
   }
 }
