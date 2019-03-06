6839b80ec281b70a22b135e7ca9685734a7d75a1
Peter Donald <peter@realityforge.org>
2019-03-07 10:58:47 +1100

    WHitespace and variable names

--- examples/src/main/java/spritz/examples/Example36.java
+++ examples/src/main/java/spritz/examples/Example36.java
@@ -7,18 +7,23 @@ public class Example36
 {
   public static void main( String[] args )
   {
-    final ConnectableStream<Integer> ticker$ = Stream
-      .periodic( 1000 )
-      .takeUntil( v11 -> v11 > 10 ).peek( v12 -> System.out.println( "Tick " + v12 ) )
-      .publish();
+    final ConnectableStream<Integer> ticker$ =
+      Stream
+        .periodic( 1000 )
+        .takeUntil( v -> v > 10 )
+        .peek( v -> System.out.println( "Tick " + v ) )
+        .publish();
 
-    ticker$.filter( v4 -> v4 % 2 == 0 )
-      .peek( v5 -> System.out.println( "T1=" + v5 ) )
+    ticker$
+      .filter( v -> v % 2 == 0 )
+      .peek( v -> System.out.println( "T1=" + v ) )
       .subscribe( new LoggingSubscriber<>() );
-    ticker$.filter( v2 -> v2 % 3 == 0 )
+    ticker$
+      .filter( v2 -> v2 % 3 == 0 )
       .peek( v3 -> System.out.println( "T2=" + v3 ) )
       .subscribe( new LoggingSubscriber<>() );
-    ticker$.filter( v -> v % 4 == 0 )
+    ticker$
+      .filter( v -> v % 4 == 0 )
       .peek( v1 -> System.out.println( "T3=" + v1 ) )
       .subscribe( new LoggingSubscriber<>() );
 
