ed093c61603846278a5f85e9b4ffc8e300fa5fbf
Peter Donald <peter@realityforge.org>
2019-03-07 17:01:49 +1100

    Make it possible to add some prefixes to logging

--- examples/src/main/java/spritz/examples/LoggingSubscriber.java
+++ examples/src/main/java/spritz/examples/LoggingSubscriber.java
@@ -1,6 +1,7 @@
 package spritz.examples;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import spritz.Scheduler;
 import spritz.Subscriber;
 import spritz.Subscription;
@@ -8,27 +9,53 @@ import spritz.Subscription;
 final class LoggingSubscriber<T>
   implements Subscriber<T>
 {
+  @Nullable
+  private final String _prefix;
+
+  LoggingSubscriber()
+  {
+    this( null );
+  }
+
+  LoggingSubscriber( @Nullable final String prefix )
+  {
+    _prefix = prefix;
+  }
+
   @Override
   public void onSubscribe( @Nonnull final Subscription subscription )
   {
-    System.out.println( "onSubscribe(" + subscription + ") on " + Scheduler.currentVpu().getName() );
+    System.out.println( prefix() + "onSubscribe(" + subscription + ")" + suffix() );
   }
 
   @Override
   public void onNext( @Nonnull final T item )
   {
-    System.out.println( "onNext(" + item + ") on " + Scheduler.currentVpu().getName() );
+    System.out.println( prefix() + "onNext(" + item + ")" + suffix() );
   }
 
   @Override
   public void onError( @Nonnull final Throwable error )
   {
-    System.out.println( "onError(" + error + ") on " + Scheduler.currentVpu().getName() );
+    System.out.println( prefix() + "onError(" + error + ")" + suffix() );
   }
 
   @Override
   public void onComplete()
   {
-    System.out.println( "onComplete() on " + Scheduler.currentVpu().getName() );
+    System.out.println( prefix() + "onComplete()" + suffix() );
   }
+
+  @Nonnull
+  private String prefix()
+  {
+    return null == _prefix ? "" : _prefix + ":";
+  }
+
+  @Nonnull
+  private String suffix()
+  {
+    return " on " + Scheduler.currentVpu().getName();
+  }
+
 }
