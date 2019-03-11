d2acf1cf1858827ad3ef9c6c540103f862a9cce2
Peter Donald <peter@realityforge.org>
2019-03-11 14:33:15 +1100

    An exception to wrap underlying error event

---
+++ core/src/main/java/spritz/WebSocketErrorException.java
@@ -0,0 +1,37 @@
+package spritz;
+
+import elemental2.dom.Event;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
+/**
+ * An error generated interacting with the WebSocket.
+ */
+public final class WebSocketErrorException
+  extends Exception
+{
+  /**
+   * The underlying error event.
+   */
+  private final Event _event;
+
+  /**
+   * Create exception to wrap underlying error event..
+   *
+   * @param event the error event.
+   */
+  public WebSocketErrorException( @Nonnull final Event event )
+  {
+    _event = Objects.requireNonNull( event );
+  }
+
+  /**
+   * Return the underlying error event.
+   *
+   * @return the underlying error event.
+   */
+  public Event getEvent()
+  {
+    return _event;
+  }
+}
